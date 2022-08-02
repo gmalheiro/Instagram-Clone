6121ead527de68588eb1bdc899165f1e2fc579e3
gmalheiro <gabrielsilvamalheiro7@.gmail.com>
2022-08-02 17:41:25 -0300

    Header component

---
+++ screens/HomeScreen.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { View, Text, StyleSheet,SafeAreaView,StatusBar} from 'react-native'
+import Header from '../components/home/Header'
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+        <Header />
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor: 'black',
+  },
+})
+
+export default HomeScreen
