3ddaa97cac529cf80e41b2bc9ad463977a90e5f5
gmalheiro <gabrielsilvamalheiro7@.gmail.com>
2022-08-03 15:14:04 -0300

    Adding Stories component and some users

---
+++ data/users.js
@@ -0,0 +1,23 @@
+export const USERS =  [
+    {
+        user: 'Michael Scott',
+        image: 
+            'https://cdn.costumewall.com/wp-content/uploads/2018/09/prison-mike.jpg',
+    },
+    {
+        user: 'Dwight Schrute',
+        image: 'https://i.pinimg.com/originals/bc/7e/e6/bc7ee65f7e1be2f7bbe4e84fa6856704.jpg',
+    },
+    {
+        user: 'Jim Halpert',
+        image: 'https://i.pinimg.com/736x/eb/62/17/eb621799fc4e486e68293ccf67f2db79--custom-suits-jim-halpert.jpg',
+    },
+    {
+        user: 'Toby Flenderson',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/a/af/PaulLieberstein.jpg',
+    },
+    {
+        user: 'Ryan Howard',
+        image:'https://64.media.tumblr.com/dead1ab37e61a09140ced3ca87a81980/2d98f5a9563d625e-1c/s400x600/3b06bf4f5cb6f1f2839b17991655987b877e034d.png',
+    }
+]
