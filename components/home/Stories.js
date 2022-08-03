7b902c0a9c04c6c5045b6fdaf1fdec0a9a3aba33
gmalheiro <gabrielsilvamalheiro7@.gmail.com>
2022-08-03 17:12:33 -0300

    Stories Component

--- components/home/Stories.js
+++ components/home/Stories.js
@@ -1,12 +1,35 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { View, Text, ScrollView, Image, StyleSheet } from 'react-native'
+import { USERS } from '../../data/users'
 
 const Stories = () => {
   return (
-    <View>
-      <Text style={{color:'white'}}>Stories</Text>
+    <View style={{marginBottom:13}} >
+        <ScrollView horizontal showsHorizontalScrollIndicator = {false}>
+            {USERS.map((story,index) => (
+            <View key={index} style={{alignItems:'center'}} >
+                <Image source = {{uri: story.image}} style={styles.story} />
+                <Text style={{color:'white'}}>
+                    {story.user.length > 11 
+                    ? story.user.slice(0,10).toLowerCase() + '...' 
+                    : story.user.toLowerCase()} 
+                </Text>
+            </View>
+            ))}
+        </ScrollView>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    story:{
+        width:70,
+        height:70,
+        borderRadius:50,
+        marginLeft: 6,
+        borderWidth:2,
+        borderColor:'#fff'
+    }
+})
 
 export default Stories
--- data/users.js
+++ data/users.js
@@ -1,23 +1,23 @@
 export const USERS =  [
     {
-        user: 'Michael Scott',
+        user: ' MichaelScott',
         image: 
             'https://cdn.costumewall.com/wp-content/uploads/2018/09/prison-mike.jpg',
     },
     {
-        user: 'Dwight Schrute',
+        user: ' DwightSchrute',
         image: 'https://i.pinimg.com/originals/bc/7e/e6/bc7ee65f7e1be2f7bbe4e84fa6856704.jpg',
     },
     {
-        user: 'Jim Halpert',
+        user: ' JimHalpert',
         image: 'https://i.pinimg.com/736x/eb/62/17/eb621799fc4e486e68293ccf67f2db79--custom-suits-jim-halpert.jpg',
     },
     {
-        user: 'Toby Flenderson',
+        user: ' TobyFlenderson',
         image: 'https://upload.wikimedia.org/wikipedia/commons/a/af/PaulLieberstein.jpg',
     },
     {
-        user: 'Ryan Howard',
-        image:'https://64.media.tumblr.com/dead1ab37e61a09140ced3ca87a81980/2d98f5a9563d625e-1c/s400x600/3b06bf4f5cb6f1f2839b17991655987b877e034d.png',
-    }
+        user: ' RyanTemp',
+        image: 'https://i.pinimg.com/originals/ce/34/86/ce3486798741c4229df0a1a1109f5ca8.jpg',
+    },
 ]
